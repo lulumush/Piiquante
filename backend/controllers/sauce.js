06ff2c4eedf93e23f9915321ae224af1401a7c3a
Lucie Sundermeijer <lucie.sun@gmail.com>
2022-10-17 11:24:27 +0200

    moving gitignore file

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -26,7 +26,7 @@ exports.createSauce = (req, res, next) => {
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })//
     .then((sauce) => { res.status(200).json(sauce)})
-    .catch(error => { res.status(400).json( { error })})//should be res.status(404) and not (400)
+    .catch(error => { res.status(404).json( { error })})
 };
 
 //modify one sauce in particular
